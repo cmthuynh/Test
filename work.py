7d47bef9d5fd7208de72d77799cc053254c1d938
cmthuynh6 <cmthuynh6@gmail.com>
2017-09-05 15:20:06 +0700

    fix

--- work.py
+++ work.py
@@ -1,17 +1,17 @@
 import numpy
 
-x = numpy.random.randint(0,100,10)
-y = numpy.random.randint(0,100,10)
+x = numpy.random.randint(0,100,100)
+y = numpy.random.randint(0,100,100)
 value = 0
-for i in range(9):
+for i in range(99):
     j=i+1
-    for j in range(10):
+    for j in range(100):
         b=y[j]-y[i]
         a=x[j]-x[i]
         if a**2 +b**2 == 0:
             continue
         count = 0
-        for k in range(10):
+        for k in range(100):
             if(b*(x[k]-x[i])-a*(y[k]-x[i])==0):
                 count=count + 1
         if value <= count:
@@ -20,5 +20,5 @@ for i in range(9):
             xf=x[i]
             yf=y[i]
             value = count
-print 'z=',bf,'*(x-',xf,')','-',af,'*(y-',yf,')'
-print 'it go through',value,'points'
+print '0 =',bf,'*(x -',xf,')','-',af,'*(y -',yf,')'
+print 'it goes through',value,'points'
